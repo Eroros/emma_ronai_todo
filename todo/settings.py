797ee841dc9c1a370f8a667677ee1e1890ef1ffc
Emma Ronai <emma.c.ronai@gmail.com>
2025-09-18 11:41:54 -0500

    resolved improper .env link, begining to implement tasks app

--- todo/settings.py
+++ todo/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR.parent / ".env")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
