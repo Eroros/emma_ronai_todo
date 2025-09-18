9d4083348aaf8f54e47569ed3642ba8af39af2b4
Emma Ronai <emma.c.ronai@gmail.com>
2025-09-18 11:45:02 -0500

    Added first model (basic)

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.timezone import now, timedelta
 
-# Create your models here.
+def default_due_date():
+    return now().date() + timedelta(days=2)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    added_on = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField(default=default_due_date, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
